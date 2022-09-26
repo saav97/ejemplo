6e88c235c473c611d005efdead72abc54bb68cf6
saav97 <sa.arriagada97@gmail.com>
2022-09-26 20:00:11 -0300

    cambios clase 26/09

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,10 +2,11 @@ const {Router} = require('express');
 const { check } = require('express-validator');
 const { getUsers, addUser } = require('../controllers/users.controller');
 const { validarCampo } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get('/', getUsers);
+router.get('/', validarJWT, getUsers);
 router.post('/',[
     check('username', 'Username es requerido').notEmpty(),
     check('email', 'Debe ser un email valido').isEmail(),
